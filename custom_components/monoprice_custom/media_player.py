722fe08e8946b06bc9dc84e0cdb3bdea40fc9148
Brad Sanders <thebradleysanders@users.noreply.github.com>
2023-03-17 14:47:36 -0500

    Update media_player.py

--- custom_components/monoprice_custom/media_player.py
+++ custom_components/monoprice_custom/media_player.py
@@ -192,10 +192,10 @@ class MonopriceZone(MediaPlayerEntity):
         self._attr_has_entity_name = True
         self._attr_name = f"Zone {self._zone_id}"
         self._attr_device_info = DeviceInfo(
-            identifiers={(DOMAIN, self.unique_id)},
+            identifiers={(DOMAIN, self._zone_id)},
             manufacturer="Monoprice",
             model="6-Zone Amplifier",
-            name=self.name,
+            name=f"Zone {self._zone_id}"
         )
         self._attr_sound_mode_list = ["Normal", "High Bass", "Medium Bass", "Low Bass"]
 
